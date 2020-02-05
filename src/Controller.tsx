f0a1571ff79b8f36ad0260e975c3bc6361e2ba38
Vasiliy Lector <vasiliy.lector@gmail.com>
2020-02-05 16:28:48 +0300

    props.props -> ...props

--- src/Controller.tsx
+++ src/Controller.tsx
@@ -8,43 +8,43 @@ interface LocalControllerClass<P, S, VP, C extends LocalController<P, S, VP> = L
     new(...args: any[]): C;
 }
 type ControllerClass<P, S, VP, SS = {}> = GlobalControllerClass<P, S, VP, SS> | LocalControllerClass<P, S, VP>;
-type ReactFuntionOrClass<P> = FC<P> | ComponentClass<P, any>;
-export type ControllerProps<P, VP, SS> = {
-    props: P,
+type ReactFunctionOrClass<P> = FC<P> | ComponentClass<P, any>;
+export type ControllerProps<P, VP, SS> = P & {
     state: SS,
     getState: GetState<SS>,
     setState: SetState<SS>,
-    Component: ReactFuntionOrClass<VP>
+    Component: ReactFunctionOrClass<VP>
 };
-export type LocalControllerProps<P, VP> = {
-    props: P,
-    Component: ReactFuntionOrClass<VP>
+export type LocalControllerProps<P, VP> = P & {
+    Component: ReactFunctionOrClass<VP>
 };
 export abstract class Controller<P, S, VP, SS>
-    extends React.Component<ControllerProps<P, VP, SS>, S> {
+    extends React.Component<P, S> {
     isLocal: false = false;
+    props!: ControllerProps<P, VP, SS>;
 
     abstract state: S;
     abstract getProps(storeState: SS, props: P, state: S): VP;
 
 	render() {
-        const { Component, state } = this.props;
+        const { Component, state, getState, setState, ...props } = this.props as any;
 
-		return <Component {...this.getProps(state, this.props.props, this.state)}/>;
+		return <Component {...this.getProps(state, props, this.state)}/>;
 	}
 }
 
 export abstract class LocalController<P, S, VP>
-    extends React.Component<LocalControllerProps<P, VP>, S> {
+    extends React.Component<P, S> {
+    props!: LocalControllerProps<P, VP>;
     isLocal: true = true;
 
     abstract state: S;
     abstract getProps(props: P, state: S): VP;
 
 	render() {
-        const { Component } = this.props;
+        const { Component, ...props } = this.props as any;
 
-		return <Component {...this.getProps(this.props.props, this.state)}/>;
+		return <Component {...this.getProps(props, this.state)}/>;
 	}
 }
 
@@ -54,21 +54,21 @@ const isLocalController = <P, S, VP, SS>(Wrapper: ControllerClass<P, S, VP, SS>)
     return Wrapper.prototype instanceof LocalController;
 }
 
-export type Enhancer<P, VP> = (Component: ReactFuntionOrClass<VP>) => ReactFuntionOrClass<P>;
+export type Enhancer<P, VP> = (Component: ReactFunctionOrClass<VP>) => ReactFunctionOrClass<P>;
 
 export const createEnhancer = <P, S, VP, SS = void>(Wrapper: ControllerClass<P, S, VP, SS>): Enhancer<P, VP> =>
-    (Component: ReactFuntionOrClass<VP>): FC<P> =>
+    (Component: ReactFunctionOrClass<VP>): FC<P> =>
         (props: P) => isLocalController(Wrapper)
             ? <Wrapper
-                props={props}
+                {...props}
                 Component={Component}
             />
             : <StoreContext.Consumer>
                 {({ state, getState, setState}) => <Wrapper
+                    {...props}
                     state={state}
                     getState={getState}
                     setState={setState}
-                    props={props}
                     Component={Component}
                 />}
             </StoreContext.Consumer>;
@@ -79,7 +79,7 @@ type EnhancersForCompose<P, VP, A = any, B = any, C = any> =
     [Enhancer<P, A>, Enhancer<A, B>, Enhancer<B, VP>] |
     [Enhancer<P, A>, Enhancer<A, B>, Enhancer<B, C>, Enhancer<C, VP>];
 
-export const compose = <P, VP>(...enhancers: EnhancersForCompose<P, VP>) => (Component: ReactFuntionOrClass<VP>) => {
+export const compose = <P, VP>(...enhancers: EnhancersForCompose<P, VP>) => (Component: ReactFunctionOrClass<VP>) => {
     let result: any = Component;    
 
     for (let i = enhancers.length - 1; i > 0; i--) {
