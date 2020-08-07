982e2b8c8199cbfec2423d23c59a787dae017033
Vasiliy Lector <vasiliy.lector@gmail.com>
2020-08-07 20:36:52 +0300

    ProvidedControllerProps

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,9 @@ export { Controller, ControllerProps } from './Controller';
 export { LocalController, LocalControllerProps } from './LocalController';
 export { Enhancer } from './types';
 export { createEnhancer, createEnhancer as create } from './createEnhancer';
-export { provideControllerProps, provideControllerProps as provide } from './provideControllerProps';
+export {
+    provideControllerProps,
+    ProvidedControllerProps,
+    provideControllerProps as provide
+} from './provideControllerProps';
 export { compose } from './compose';
--- src/provideControllerProps.tsx
+++ src/provideControllerProps.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { StoreContext, GetState, GetMomentState, SetState } from './Store';
 import { ReactFunctionOrClass } from './types';
 
-export const provideControllerProps = <P extends {} = any, SS = any>(Component: ReactFunctionOrClass<P & {
+export type ProvidedControllerProps<P, SS> = P & {
     state: SS,
     getState: GetState<SS>,
     getMomentState: GetMomentState<SS>,
     setState: SetState<SS>
-}>) =>
+};
+
+export const provideControllerProps =
+    <P extends {} = any, SS = any>(Component: ReactFunctionOrClass<ProvidedControllerProps<P, SS>>) =>
         function ControllerPropsProvider(props: P) {
             return <StoreContext.Consumer>
                 {({ state, getState, setState, getMomentState }) => <Component
